c46a946ce6537684f1c3de9c665748b48f027ebe
Emiliano Prediger <epredigervianello@santandertecnologia.com.ar>
2022-06-13 01:56:54 -0300

    refactor código duplicado a módulo comunicación

--- banco/src/main.rs
+++ banco/src/main.rs
@@ -2,43 +2,17 @@ use std::io::{Read, Write};
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
+
 use rand::thread_rng;
 use rand::Rng;
+
+use comunicacion::{Error, Pay, Succesfull, Unknown};
 
 static SERVER_ARGS: usize = 2;
 
 const DEFAULT_PORT: &str = "7666";
 const DEFAULT_IP: &str = "127.0.0.1";
 const ERROR: u8 = 1;
-
-pub enum Message {
-    Pay,
-    Succesfull,
-    Error,
-    Unknown
-}
-
-impl From<u8> for Message {
-    fn from(code: u8) -> Message {
-        match code & 0xF0 {
-            0x00 => Message::Pay,
-            0x10 => Message::Succesfull,
-            0x20 => Message::Error,
-            _ => Message::Unknown
-        }
-    }
-}
-
-impl From<Message> for u8 {
-    fn from(code: Message) -> u8 {
-        match code {
-            Message::Pay => 0x00,
-            Message::Succesfull => 0x10,
-            Message::Error => 0x20,
-            _ => 0x99
-        }
-    }
-}
 
 pub fn get_address() -> String {
     format!("{}:{}", DEFAULT_IP, DEFAULT_PORT)
@@ -68,23 +42,23 @@ fn wait_new_clients(address: &str) -> std::io::Result<()> {
 
 fn procesamiento_aleatorio() {
     const FACTOR_TEMPORAL: u64 = 3;
-    let ms = thread_rng().gen_range(1000, 3000);
+    let ms = thread_rng().gen_range(100, 300);
     thread::sleep(Duration::from_millis(ms * FACTOR_TEMPORAL));
 }
 
 fn pago_es_correcto() -> bool {
+    const UMBRAL_ERROR: i32 = 500;
+
     procesamiento_aleatorio();
+
     let valor = thread_rng().gen_range(0, 1000);
-    if valor > 500 {
-        true
-    } else {
-        false
-    }
+
+    valor > UMBRAL_ERROR
 }
 
 fn send_succesfull_message(stream: &mut TcpStream) {
-    let buffer = [Message::Succesfull.into(), 0_u8];
-    match stream.write_all(&buffer){
+    let buffer = [Succesfull.into(), 0_u8];
+    match stream.write_all(&buffer) {
         Ok(_) => {
             println!("<BANCO> Mensaje enviado correctamente!");
         }
@@ -95,8 +69,8 @@ fn send_succesfull_message(stream: &mut TcpStream) {
 }
 
 fn send_error_message(stream: &mut TcpStream) {
-    let buffer = [Message::Error.into(), 0_u8];
-    match stream.write_all(&buffer){
+    let buffer = [Error.into(), 0_u8];
+    match stream.write_all(&buffer) {
         Ok(_) => {
             println!("<BANCO> Mensaje enviado correctamente!");
         }
@@ -114,12 +88,12 @@ fn bytes2string(bytes: &[u8]) -> Result<String, u8> {
 }
 
 fn read_pay(buffer_packet: Vec<u8>) -> String {
-    let mut _index = 0 as usize;
-    let mut _aux: String;
+    let mut _index = 0_usize;
+
     let pago_size: usize = buffer_packet[(_index) as usize] as usize; //esto es asi porque los string en su primer byte tiene el tamaño, seguido del contenido
-    _index += 1 as usize;
-    _aux = bytes2string(&buffer_packet[_index..(_index + pago_size)]).unwrap(); //falta manejar
-    _aux
+    _index += 1;
+
+    bytes2string(&buffer_packet[_index..(_index + pago_size)]).unwrap()
 }
 
 fn read_packet_from_client(stream: &mut TcpStream) {
@@ -130,17 +104,20 @@ fn read_packet_from_client(stream: &mut TcpStream) {
                 let message_type = num_buffer[0].into(); //Primer byte es el tipo de mensaje
                 let size = num_buffer[1]; //El segundo es el tamaño
 
-                let mut buffer_packet: Vec<u8> = vec![0; size as usize];//Me creo un contenedor del tamaño q me dijeron
+                let mut buffer_packet: Vec<u8> = vec![0; size as usize]; //Me creo un contenedor del tamaño q me dijeron
                 let _aux = stream.read_exact(&mut buffer_packet); //Leo lo que me dijeron que lea
-                match message_type{
-                    Message::Pay => {
+                match message_type {
+                    Pay => {
                         println!("<BANCO> Recibi un pago, voy a procesarlo!");
                         let aux = read_pay(buffer_packet);
                         if pago_es_correcto() {
                             println!("<BANCO> El Pago de {}$ fue recibido adecuadamente.", aux);
                             send_succesfull_message(stream);
                         } else {
-                            println!("<BANCO> Tuvimos un problema al validar el pago de {}$.", aux);
+                            println!(
+                                "<BANCO> Tuvimos un problema al validar el pago de {}$.",
+                                aux
+                            );
                             send_error_message(stream);
                         }
                     }
--- procesador_pagos/src/main.rs
+++ procesador_pagos/src/main.rs
@@ -4,12 +4,13 @@ use std::io::{BufRead, BufReader};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
+use comunicacion::Message;
+use comunicacion::{Error, Pay, Succesfull};
+
 fn push_to_buffer(buffer: &mut Vec<u8>, data: String) {
     buffer.push(data.len() as u8);
-    let data_bytes = data.as_bytes();
-    for i in 0..data_bytes.len() {
-        buffer.push(data_bytes[i]);
-    }
+
+    buffer.extend_from_slice(data.as_bytes());
 }
 
 #[allow(dead_code)]
@@ -18,35 +19,6 @@ struct PaqueteTuristico {
     precio: u32,
     vuelo: String,
     hotel: String,
-}
-
-pub enum Message {
-    Pay,
-    Succesfull,
-    Error,
-    Unknown
-}
-// Fijarse si se puede usar un crate!
-impl From<u8> for Message {
-    fn from(code: u8) -> Message {
-        match code & 0xF0 {
-            0x00 => Message::Pay,
-            0x10 => Message::Succesfull,
-            0x20 => Message::Error,
-            _ => Message::Unknown
-        }
-    }
-}
-
-impl From<Message> for u8 {
-    fn from(code: Message) -> u8 {
-        match code {
-            Message::Pay => 0x00,
-            Message::Succesfull => 0x10,
-            Message::Error => 0x20,
-            _ => 0x99
-        }
-    }
 }
 
 /// Módulo de Procesamiento de Pagos
@@ -61,24 +33,27 @@ fn main() {
 
     let paquetes_turisticos = parsear_paquetes(ruta);
 
-    let address = format!("127.0.0.1:7666");
+    let address = "127.0.0.1:7666".to_string();
     println!("Conectado a ... {}", address);
-    let mut stream = TcpStream::connect(address).expect("<CLIENTE>No me pude conectar al Banco!"); //Falta manejar el error
+    let mut stream = TcpStream::connect(address).expect("<CLIENTE> No me pude conectar al Banco!"); //Falta manejar el error
 
     for paquetes_turistico in paquetes_turisticos {
-        enviar_pago_a_banco(&mut stream, paquetes_turistico.precio);
+        enviar_pago_a_banco(&mut stream, paquetes_turistico);
         leer_respuesta(&mut stream);
     }
 }
 
-fn enviar_pago_a_banco(stream: &mut TcpStream, monto: u32) {
-    let cantidad_pago = monto.to_string();
+fn enviar_pago_a_banco(stream: &mut TcpStream, paquete: PaqueteTuristico) {
+    let cantidad_pago = paquete.precio.to_string();
 
     let size = (cantidad_pago.len() + 1) as u8;
-    let buffer = [Message::Pay.into(), size]; //Me armo el buffer de aviso, primer byte tipo, segundo byte tamaño
+    let buffer = [Pay.into(), size]; // Me armo el buffer de aviso, primer byte tipo, segundo byte tamaño
     match stream.write_all(&buffer) {
         Ok(_) => {
-            println!("<Cliente> Mensaje enviado correctamente!");
+            println!(
+                "<Cliente> Mensaje (id: {}) enviado correctamente!",
+                paquete.id
+            );
         }
         Err(_) => {
             println!("<Cliente> No me pude contactar con el banco!");
@@ -86,11 +61,14 @@ fn enviar_pago_a_banco(stream: &mut TcpStream, monto: u32) {
         }
     }
 
-    let mut buffer_envio: Vec<u8> = Vec::with_capacity(size.into()); //Aca me armo el buffer con el contenido del mensaje, en este caso solo me meto los "500" que quiero pagar
+    let mut buffer_envio: Vec<u8> = Vec::with_capacity(size.into()); // Aca me armo el buffer con el contenido del mensaje, en este caso solo me meto los "500" que quiero pagar
     push_to_buffer(&mut buffer_envio, cantidad_pago);
     match stream.write(&buffer_envio) {
         Ok(_) => {
-            println!("<Cliente> Mensaje enviado correctamente!");
+            println!(
+                "<Cliente> Mensaje (id: {}) enviado correctamente!",
+                paquete.id
+            );
         }
         Err(_) => {
             println!("<Cliente> No me pude contactar con el banco!");
@@ -102,19 +80,19 @@ pub fn leer_respuesta(stream: &mut TcpStream) {
     let mut num_buffer = [0u8; 2];
     let _aux = stream.read_exact(&mut num_buffer);
     match Message::from(num_buffer[0]) {
-        Message::Succesfull => {
+        Succesfull => {
             println!("<Cliente> Pago procesado correctamente!\n");
         }
-        Message::Error => {
-            println!("<Cliente> No pudo procesarce el pago!\n");
+        Error => {
+            println!("<Cliente> No pudo procesarse el pago!\n");
         }
         _ => {
-            println!("<Cliente>No se que me contesto el server!\n");
+            println!("<Cliente> No sé qué me contestó el server!\n");
         }
     }
 }
 
-fn parsear_paquetes(ruta: &String) -> Vec<PaqueteTuristico> {
+fn parsear_paquetes(ruta: &str) -> Vec<PaqueteTuristico> {
     let mut paquetes_turisticos: Vec<PaqueteTuristico> = Vec::new();
 
     match abrir_archivo_paquetes(ruta) {
@@ -131,7 +109,9 @@ fn parsear_paquetes(ruta: &String) -> Vec<PaqueteTuristico> {
                             hotel: String::from(paquete[3]),
                         })
                     }
-                    Err(e) => { eprintln!("Ups... {}", e) }
+                    Err(e) => {
+                        eprintln!("Ups... {}", e)
+                    }
                 }
             }
         }
@@ -139,16 +119,13 @@ fn parsear_paquetes(ruta: &String) -> Vec<PaqueteTuristico> {
             eprintln!("Error: {}", err);
         }
     }
-    return paquetes_turisticos;
+
+    paquetes_turisticos
 }
 
 fn abrir_archivo_paquetes(ruta: &str) -> Result<BufReader<File>, std::io::Error> {
     match File::open(ruta) {
-        Ok(archivo_pagos) => {
-            Ok(BufReader::new(archivo_pagos))
-        }
-        Err(err) => {
-            Err(err)
-        }
+        Ok(archivo_pagos) => Ok(BufReader::new(archivo_pagos)),
+        Err(err) => Err(err),
     }
 }
